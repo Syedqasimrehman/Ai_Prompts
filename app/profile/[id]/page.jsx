f2319b7cd575dc468157031051e58396ac5d802d
syed qasim <qasimsyed563@gmail.com>
2025-06-24 00:23:16 +0500

    fixing

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,34 +1,48 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-
-import Profile from "@components/Profile";
-
-const UserProfile = ({ params }) => {
-  const searchParams = useSearchParams();
-  const userName = searchParams.get("name");
-
-  const [userPosts, setUserPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${params?.id}/posts`);
-      const data = await response.json();
-
-      setUserPosts(data);
-    };
-
-    if (params?.id) fetchPosts();
-  }, [params.id]);
-
+import { Suspense } from "react";
+import UserProfile from "@components/UserProfile";
+import Image from "@node_modules/next/image";
+import Loading from "../Loading";
+const UserProfilePage = ({ params }) => {
   return (
-    <Profile
-      name={userName}
-      desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
-      data={userPosts}
-    />
+    <Suspense fallback={<Loading />}>
+      <UserProfile params={params} />
+    </Suspense>
   );
 };
+export default UserProfilePage;
+// "use client";
 
-export default UserProfile;
+// import { useEffect, useState, Suspense } from "react";
+// import { useSearchParams } from "next/navigation";
+
+// import Profile from "@components/Profile";
+
+// const UserProfile = ({ params }) => {
+//   const searchParams = useSearchParams();
+//   const userName = searchParams.get("name");
+
+//   const [userPosts, setUserPosts] = useState([]);
+
+//   useEffect(() => {
+//     const fetchPosts = async () => {
+//       const response = await fetch(`/api/users/${params?.id}/posts`);
+//       const data = await response.json();
+
+//       setUserPosts(data);
+//     };
+
+//     if (params?.id) fetchPosts();
+//   }, [params.id]);
+
+//   return (
+//     <Suspense>
+//       <Profile
+//         name={userName}
+//         desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+//         data={userPosts}
+//       />
+//     </Suspense>
+//   );
+// };
+
+// export default UserProfile;
