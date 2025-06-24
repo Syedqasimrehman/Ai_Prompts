c39c66feaa08a8c14b0b98ff5b40f3359c103d5a
syed qasim <qasimsyed563@gmail.com>
2025-06-24 10:43:46 +0500

    add

---
+++ app/update-prompt/Susepense.jsx
@@ -0,0 +1,12 @@
+import {Suspense} from 'react'
+import EditPrompt from './page'
+
+const Susepense = () => {
+  return (
+    <Susepense>
+        <EditPrompt />
+    </Susepense>
+  )
+}
+
+export default Susepense
