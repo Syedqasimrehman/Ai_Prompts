5421ca5707e304c31a89abe1c3348ed6a6df69ee
syed qasim <qasimsyed563@gmail.com>
2025-06-24 10:48:35 +0500

    change

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,14 +1,14 @@
 "use client";
 
-import { useEffect, useState ,Suspense } from "react";
-import { useRouter ,useSearchParams } from "@node_modules/next/navigation"; 
+import { useEffect, useState, Suspense } from "react";
+import { useRouter, useSearchParams } from "@node_modules/next/navigation";
 import Form from "@components/Form";
 
 const EditPrompt = () => {
   const router = useRouter();
-  
+
   const searchPrams = useSearchParams();
-  const promptId = searchPrams.get('id');
+  const promptId = searchPrams.get("id");
 
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
@@ -17,23 +17,23 @@ const EditPrompt = () => {
   });
 
   useEffect(() => {
-    const getPromptDetails = async () =>{
-     const response = await fetch(`/api/prompt/${promptId}`)
-     const data= await response.json();
+    const getPromptDetails = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
 
-     setPost({
+      setPost({
         prompt: data.prompt,
         tag: data.tag,
-     })
-    }
-    if(promptId) getPromptDetails();
-  }, [promptId])
-  
+      });
+    };
+    if (promptId) getPromptDetails();
+  }, [promptId]);
+
   const updatePrompt = async (e) => {
     e.preventDefault();
     setSubmitting(true);
 
-    if(!promptId) return alert('Prompt ID not found')
+    if (!promptId) return alert("Prompt ID not found");
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -55,13 +55,27 @@ const EditPrompt = () => {
   };
 
   return (
-    <Form
-      type="Edit"
-      post={post}
-      setPost={setPost}
-      submitting={submitting}
-      handleSubmit={updatePrompt}
-    />
+    <Suspense
+      fallback={
+        <div className="w-full flex-center">
+          <Image
+            src="assets/icons/loader.svg"
+            width={50}
+            height={50}
+            alt="loader"
+            className="object-contain"
+          />
+        </div>
+      }
+    >
+      <Form
+        type="Edit"
+        post={post}
+        setPost={setPost}
+        submitting={submitting}
+        handleSubmit={updatePrompt}
+      />
+    </Suspense>
   );
 };
 
